69f6027533f753c18703a02dca146488b94de779
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2023-04-26 20:51:38 +0530

    Add files via upload

---
+++ Arrays_init_trav_class_dyn.cpp
@@ -0,0 +1,45 @@
+//write an dynamic array with new keyword with class
+#include <iostream>                 
+using namespace std;
+
+class Array
+{
+
+    int *a;
+    int size;
+public:
+    Array(int b)
+    {
+        size = b;
+        a = new int[size];
+        //Array Traversal
+        for (int i = 0; i < size; i++)
+        {
+            a[i] = i;
+        }
+    }
+
+    ~Array()
+    {
+        delete[] a;
+        cout << "Destructor called" << endl;
+    }
+
+    void print()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            cout << a[i] << endl;
+        }
+
+        free(a);
+    }
+};
+
+int main()
+{
+    Array arr(5);
+    arr.print();
+
+    return 0;
+}
