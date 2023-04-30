445c0576981b2f52ae09f918bd7461a38a18c51f
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2023-05-01 03:54:03 +0530

    Add files via upload

---
+++ Arrays_Three_Dimensional_1.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+using namespace std;
+
+class Array
+{
+
+private:
+    int ***a;
+    int row;
+    int col;
+    int page;
+
+public:
+
+    Array(int ***a, int page, int row, int col)
+    {
+
+        this->a = a;
+        this->row = row;
+        this->col = col;
+        this->page = page;
+
+        // Allocating memory for the pages
+        a = (int ***)malloc(page * sizeof(int **));
+
+        // Allocating memory for the rows
+        for (int i = 0; i < page; i++)
+        {
+            a[i] = (int **)malloc(row * sizeof(int *));
+        }
+
+        // Allocating memory for the columns
+        for (int i = 0; i < page; i++)
+        {
+            for (int j = 0; j < row; j++)
+            {
+                a[i][j] = (int *)malloc(col * sizeof(int));
+            }
+        }
+
+        // Enter the elements of the array
+
+        cout << "Enter the elements of the array: " << endl;
+
+        for (int i = 0; i < page; i++)
+        {
+            for (int j = 0; j < row; j++)
+            {
+                for (int k = 0; k < col; k++)
+                {
+                    cout << "a[" << i << "][" << j << "][" << k << "] = ";
+                    cin >> *(*(*(a + i) + j) + k);
+                }
+            }
+        }
+
+        // Printing the array
+
+        cout << "The array is: " << endl;
+
+        for (int i = 0; i < page; i++)
+        {
+            for (int j = 0; j < row; j++)
+            {
+                for (int k = 0; k < col; k++)
+                {
+                    cout << "a[" << i << "][" << j << "][" << k << "] = ";
+                    cout << *(*(*(a + i) + j) + k) << " ,";
+                }
+                cout << endl;
+            }
+            cout << endl;
+        }                           
+       
+    }
+
+    ~Array()
+    {
+
+        cout << "Array deleted" << endl;
+        cout << "Destructor is called!" << endl;
+
+
+        // Deallocating memory for the columns
+        for (int i = 0; i < page; i++)
+        {
+            for (int j = 0; j < row; j++)
+            {
+                delete[] a[i][j];
+            }
+        }
+
+        // Deallocating memory for the rows
+        for (int i = 0; i < page; i++)
+        {
+            delete[] a[i];
+        }
+
+        // Deallocating memory for the pages
+        delete[] a;
+
+       
+    }
+};
+
+
+int main()
+{
+
+    int page = 2;
+    int row = 2;
+    int col = 2;
+    int myarr[page][row][col];
+
+    Array arr((int***)myarr, page, row, col);
+
+    return 0;
+}
---
+++ Arrays_Two_Dimensional_1.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+using namespace std;
+
+class Array
+{
+private:
+    int **a;
+    int row;
+    int col;
+    
+
+public:
+
+    Array(int **a, int row, int col)
+    {
+        this->a = a;
+        this->row = row;
+        this->col = col;
+       
+        // Allocating memory for the rows
+
+        a = (int **)malloc(row * sizeof(int *));
+
+        // Allocating memory for the columns
+        for (int i = 0; i < row; i++)
+        {
+            a[i] = (int *)malloc(col * sizeof(int));
+        }
+
+        // Enter the elements of the array
+
+        cout << "Enter the elements of the array: " << endl;
+
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+
+                cout << "a[" << i << "][" << j << "] = ";
+                cin >> *(*(a + i) + j);                 
+            }
+        }
+
+        // Printing the array
+
+        cout << "The array is: " << endl;
+
+
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+
+                cout << "a[" << i << "][" << j << "] = ";
+                cout << *(*(a + i) + j) << " ,";
+            }
+            cout << endl;
+
+
+    }
+
+
+    }
+
+
+    ~Array()
+    {
+
+    cout << "Array deleted" << endl;
+    cout << "Destructor is called!" << endl;
+
+    // Deallocating the memory
+    // Deleting the columns
+    for (int i = 0; i < row; i++)
+    {
+            delete[] a[i];
+        }
+        
+        //Deleting the rows
+        delete[] a;
+        
+    }
+};
+
+int main()
+{
+
+    int row = 2;
+    int col = 2;
+    int myarr[row][col] ;
+    
+    Array arr((int**)myarr, row, col);
+
+    
+
+    return 0;
+}
+
